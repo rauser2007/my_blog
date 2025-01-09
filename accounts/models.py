387549cab2579692423ce48a0412c47319aaed26
Дмитро <murchyk2007@gmail.com>
2025-01-09 12:31:11 +0200

    created accounts models

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,5 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
+class CustomUser(AbstractUser):
+    bio = models.TextField(blank=True, null=True)
