d43aa783837401eb6fda473f4f51275f5a513900
Дмитро <murchyk2007@gmail.com>
2025-01-17 19:10:58 +0200

    fix

--- articles/urls.py
+++ articles/urls.py
@@ -5,8 +5,8 @@ app_name = 'articles'
 
 urlpatterns = [
     path('', views.ArticleListView.as_view(), name='article-list'),  # Список статей
-    path('<int:pk>', views.ArticleDetailView.as_view(), name='article-detail'),  # Деталі статті
-    path('create/', views.ArticleCreateView.as_view(), name='article-create'),  # Створення статті
-    path('update/<int:pk>/', views.ArticleUpdateView.as_view(), name='article-update'),  # Редагування статті
-    path('delete/<int:pk>/', views.ArticleDeleteView.as_view(), name='article-delete'),  # Видалення статті
+    path('artical/<int:pk>/', views.ArticleDetailView.as_view(), name='article-detail'),  # Деталі статті
+    path('create/artical/', views.ArticleCreateView.as_view(), name='article-create'),  # Створення статті
+    path('update/artical/<int:pk>/', views.ArticleUpdateView.as_view(), name='article-update'),  # Редагування статті
+    path('delete/artical/<int:pk>/', views.ArticleDeleteView.as_view(), name='article-delete'),  # Видалення статті
 ]
